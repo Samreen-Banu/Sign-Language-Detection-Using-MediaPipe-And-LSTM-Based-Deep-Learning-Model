00a7360372bfc74a7a3d5baa71a5cf392c0b7b6b
Samreen Banu <samreenbanu2511@gmail.com>
2025-09-09 00:37:07 +0530

    first commit

---
+++ trainmodel.py
@@ -0,0 +1,52 @@
+from function import *
+from sklearn.model_selection import train_test_split
+from keras.utils import to_categorical
+from keras.models import Sequential
+from keras.layers import LSTM, Dense
+from keras.callbacks import TensorBoard
+import numpy as np
+import os
+
+label_map = {label: num for num, label in enumerate(actions)}
+sequences, labels = [], []
+
+for action in actions:
+    for sequence in range(no_sequences):
+        window = []
+        for frame_num in range(sequence_length):
+            path = os.path.join(DATA_PATH, action, str(sequence), f"{frame_num}.npy")
+            try:
+                res = np.load(path, allow_pickle=True)
+                if res is None or not isinstance(res, np.ndarray) or res.shape != (63,):
+                    continue
+                window.append(res)
+            except:
+                continue
+        if len(window) == sequence_length:
+            sequences.append(window)
+            labels.append(label_map[action])
+
+X = np.array(sequences)
+y = to_categorical(labels).astype(int)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.05)
+
+log_dir = os.path.join('Logs')
+tb_callback = TensorBoard(log_dir=log_dir)
+
+model = Sequential()
+model.add(LSTM(64, return_sequences=True, activation='relu', input_shape=(sequence_length, 63)))
+model.add(LSTM(128, return_sequences=True, activation='relu'))
+model.add(LSTM(64, return_sequences=False, activation='relu'))
+model.add(Dense(64, activation='relu'))
+model.add(Dense(32, activation='relu'))
+model.add(Dense(len(actions), activation='softmax'))
+
+model.compile(optimizer='Adam', loss='categorical_crossentropy', metrics=['categorical_accuracy'])
+model.fit(X_train, y_train, epochs=200, callbacks=[tb_callback])
+model.summary()
+
+# Save model
+model_json = model.to_json()
+with open("model.json", "w") as json_file:
+    json_file.write(model_json)
+model.save('model.h5')
